6bb4e9645cccbc9d2de38756d5a981baa6e3dd93
Abraham V. <abraham.varricatt@googlemail.com>
2025-08-31 16:40:38 -0400

    try pulling other branch from CICD

--- build.sh
+++ build.sh
@@ -4,4 +4,11 @@ echo "running build"
 
 git status
 
+# pull branch v4_personal
+git remote set-branches origin 'v4_personal'
+git fetch --depth 1 origin v4_personal
+git checkout v4_personal
 
+ls # hope this shows everything?
+
+cat quartz.config.ts  # to validate it exists
