0b558c99e2b82e10fde84445a12d36f5c16426b6
Abraham V. <abraham.varricatt@googlemail.com>
2025-08-31 17:11:07 -0400

    update titles in config

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,15 +8,13 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "Note 2 Self",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
-    analytics: {
-      provider: "plausible",
-    },
+    analytics: null,
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "obsidian-quartz-6a3.pages.dev",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
