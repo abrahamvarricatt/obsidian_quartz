857ef6bc4743ad73e460d4997307513f633fd7c5
Abraham V. <abraham.varricatt@googlemail.com>
2025-08-31 16:48:07 -0400

    build and deploy
    building my quartz site in an unconventional manner. :)

--- build.sh
+++ build.sh
@@ -11,4 +11,24 @@ git checkout v4_personal
 
 ls # hope this shows everything?
 
+echo "---"
 cat quartz.config.ts  # to validate it exists
+echo "--- quartz.config.ts OVER"
+
+echo "purging dummy content folder"
+rm -rf content
+
+echo "copy content folder from public_notes branch"
+git checkout public_notes -- content
+
+# list node and npm versions
+node --version
+npm --version
+
+echo "install npm dependencies"
+npm ci
+
+echo "build quartz"
+npx quartz build
+
+# at this point, I hope the public folder will be available for deployment.
